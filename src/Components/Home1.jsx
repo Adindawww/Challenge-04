7fd6f4a88e8559192830a3883e1aae2643822318
adindawww <adindawahyu680@gmail.com>
2023-10-07 23:36:55 +0700

    Update Home1.jsx

--- src/Components/Home1.jsx
+++ src/Components/Home1.jsx
@@ -42,7 +42,7 @@ export const Home1 = () => {
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container-fluid">
                     <a className="navbar-judul">
-                        <h1><b>Film Baru</b></h1>
+                        <h1><b>MovieList</b></h1>
                     </a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
